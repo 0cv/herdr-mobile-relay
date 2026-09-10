4f10a653cdf656433c76a8c85b8c83c383c13df7
Christophe Vidal <cvidal@mavens.com>
2026-09-10 08:35:02 +0000

    Regenerate web release bundle from final frontend source

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.21.0-369-88ab996bd472cfa9/index.html", location.origin);
+  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.21.0-370-9a92888d5b0f677d/index.html", location.origin);
   target.search = location.search;
   target.hash = location.hash;
   location.replace(target.href);
