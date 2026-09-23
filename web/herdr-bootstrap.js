9d1c9a39bda6ebb4733853cdc4c7d3518cdc5d35
Christophe Vidal <cvidal@mavens.com>
2026-09-23 05:37:35 +0000

    chore: regenerate committed web release bundle

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,4 +1,4 @@
-const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.21.3-387-b369518bfd47989a/index.html", location);
+const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.21.3-388-ef4542f819566d18/index.html", location);
   e.search = location.search;
   e.hash = location.hash;
   location.replace(e);
